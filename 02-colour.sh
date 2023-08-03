55a2f8c1f98c76d316f8e5fd43a9aa152fccb00a
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-03 18:14:03 +0530

    Auto Push

--- 02-colour.sh
+++ 02-colour.sh
@@ -1,2 +1,2 @@
-echo -e "e\ [33m I am printing yellow colour \e [0m"
-echo -e "e\ [35m I am learning Devops with aws \e [0m"
+echo -e"e\[33m I am printing yellow colour \e[0m"
+echo -e"e\[35m I am learning Devops with aws \e[0m"
