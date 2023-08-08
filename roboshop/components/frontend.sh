d257cc8a7994b1ff5b7d39c54b64b3fae7b75e52
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-08 21:41:17 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ] ; then
+echo -e "\e[32m script is executed by the root user or with a sudo privilage \e[0m \n \t Example : sudo bash wrapper.sh frontend"
+exit 1
+fi
 echo "configuring frontend"
-yum install nginx -y
+yum install nginx -y  &>> /tmp/frontend.log
 
+#validate the user who is running the script is a root user or not
 
+# I want to ensure , that the SCRIPT SHOULD FAIL the user who run the script is not a root user
+# rather executing the commands and failing
+ 
 
 # yum install nginx -y
 # systemctl enable nginx
