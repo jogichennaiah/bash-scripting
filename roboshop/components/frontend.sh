8a0e596fdbcef90747976b604bafea8a3a58c937
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-08 21:54:37 +0530

    Auto Push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,6 +5,7 @@ echo -e "\e[32m script is executed by the root user or with a sudo privilage \e[
 exit 1
 fi
 echo -e "\e[34m configuring frontend.......! \e[0m"
+echo "installing  frontend : "
 yum install nginx -y  &>> /tmp/frontend.log
 
 #validate the user who is running the script is a root user or not
