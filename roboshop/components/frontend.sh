42b907a0dad693f9d5fb9b05a3dc31b4dab16ae6
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-09 19:05:12 +0530

    AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -28,7 +28,7 @@ systemctl start nginx   ${LOGFILE}
 stat $?
 
  echo -n "Downloading the ${COMPONENT} components :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 
 stat $?
 
@@ -38,10 +38,7 @@ rm -rf *    ${LOGFILE}
 stat $?
 
 echo -n "extracting the ${COMPONENT} :"
-unzip /tmp/${COMPONENT}.zip ${LOGFILE}
-stat $?
- 
-echo -n "sorting the ${COMPONENT} files :"
+unzip /tmp/frontend.zip ${LOGFILE}
  mv ${COMPONENT}-main/* .
  mv static/* .
  rm -rf ${COMPONENT}-main README.md  &>> ${LOGFILE}
