e05db75c3efe880d868b224647d40d2a686f2601
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-11 20:10:58 +0530

    Git AutoPush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -32,9 +32,9 @@ useradd roboshop
 stat $?
 fi
 
-#echo -n "Downloading the ${COMPONENT} :"
-#curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-#stat $?
+echo -n "Downloading the ${COMPONENT} :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
  
 #echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
  #cd /home/${APPUSER}/
