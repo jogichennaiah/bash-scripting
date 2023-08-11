b723932483a87a81c437ee52496b4c0dd62e3659
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-11 19:44:58 +0530

    AutoPush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -25,21 +25,21 @@ yum install  nodejs -y   &>> ${LOGFILE}
 stat $? 
 
 id ${APPUSER}  &>> ${LOGFILE}
-if [ $? -ne 0 ] ;then 
+##if [ $? -ne 0 ] ;then 
 echo -n "Creating application user account :"
-useradd roboshop  &>> ${LOGFILE}
+useradd roboshop  
 stat $?
-fi
 
-echo -n "Downloading the ${COMPONENT} :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
+
+##echo -n "Downloading the ${COMPONENT} :"
+#curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+#stat $?
  
-echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
- cd /home/${APPUSER}/
- rm -rf ${COMPONENT}  &>> ${LOGFILE}
- unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
-stat $?
+#echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
+ #cd /home/${APPUSER}/
+ #rm -rf ${COMPONENT}  &>> ${LOGFILE}
+ #unzip -o /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
+#stat $?
 
 
  
