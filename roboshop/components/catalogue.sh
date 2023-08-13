a8da49928e31811628cd0644af5fcdfa46365af9
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-13 19:24:45 +0530

    Git AutoPush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -24,19 +24,20 @@ echo -n "Installing Node Js :"
 yum install nodejs -y  &>> ${LOGFILE}
 stat $?
 
-id ${APPUSER}  &>> ${LOGFILE}
-if [ $? -ne 0 ] ; then
+#id ${APPUSER}  &>> ${LOGFILE}
+
+#if [ $? -ne 0 ] ; then
 echo -n "Creating Application user account :"
 useradd roboshop
 stat $?
-fi
+#fi
 
-echo -n "Downloading the ${COMPONENT} :"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
+#echo -n "Downloading the ${COMPONENT} :"
+#curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+#stat $?
 
-echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory :"
-cd /home/${APPUSER}/
-rm -rf ${COMPONENT}    &>> ${LOGFILE}
-unzip -o /tmp/${COMPONENT}.zip    &>> ${LOGFILE}
-stat $?
+#echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory :"
+#cd /home/${APPUSER}/
+#rm -rf ${COMPONENT}    &>> ${LOGFILE}
+#unzip -o /tmp/${COMPONENT}.zip    &>> ${LOGFILE}
+#stat $?
