98110fee07e642b1322351a2efb679ff712135bb
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-14 22:43:01 +0530

    Git AutoPush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,50 +1,44 @@
 #!/bin/bash/
-component=catalogue
-LogFile=/tmp/${component}.log
-AppUser=roboshop
-status(){
-    if [ $1 -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi
-}
-
-#Validate the user who is running the script Is a root user or not
 USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ] ; then # root user id is always 0 
-    echo -e "\e[32m Script is expected to execute by the root user or with a sudo privilege. \e[0m\nExample: \n\t sudo bash wrapper.sh"
-    exit 1
+COMPONENT=catalogue
+LOGFILE=/tmp/${COMPONENT}.log
+APPUSER=roboshop
+
+if [ $USER_ID -ne 0 ] ; then
+echo -e "\e[32m script is executed by the root user or with a sudo privilage \e[0m \n \t Example : sudo bash wrapper.sh ${COMPONENT}"
+exit 1
+
 fi
+stat() {
+if [ $1 -eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else 
+  echo -e "\e[31m failure \e[0m"
+  fi
+}
+echo -e "\e[34m configuring ${COMPONENT}.......! \e[0m"
+echo -e  -n "configuring ${COMPONENT} repo :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -    &>> ${LOGFILE}
+stat $?
 
+echo -n "Installing Node Js :"
+yum install nodejs -y  &>> ${LOGFILE}
+stat $?
 
-echo -e "\e[35m Configuring ${component} \e[0m" 
-echo -e -n "Configuring ${component} Repo : "
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>> ${LogFile}
-status $?
+id ${APPUSER}  &>> ${LOGFILE}
 
-
-echo -n Installing NodeJS :
-yum install nodejs -y &>> ${LogFile}
-status $?
-
-id ${AppUser} &>>${LogFile}
 if [ $? -ne 0 ] ; then
-    echo -n creating Application User Account :
-    useradd ${AppUser} &>> ${LogFile}
-    echo -e "\e[32m Success \e[0m"
-else
-    echo -n User account already exists : 
-    echo -e "\e[32m Success \e[0m"
+echo -n "Creating Application user account :"
+useradd roboshop
+stat $?
 fi
 
-echo -n Downloading the ${component} :
-curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/${component}/archive/main.zip" &>> ${LogFile}
-status $?
+echo -n "Downloading the ${COMPONENT} :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
 
-echo -n copying the ${component} to ${AppUser} home directory : 
-cd /home/${AppUser}/
-rm -rf ${component}
-unzip -o /tmp/${component}.zip &>> ${LogFile}
-status $?
+echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory :"
+cd /home/${APPUSER}/
+rm -rf ${COMPONENT}    &>> ${LOGFILE}
+unzip -o /tmp/catalogue.zip    &>> ${LOGFILE}
+stat $?
