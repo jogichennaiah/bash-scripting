0a8fe33a5e0878b4d88368640ea82d891d97c48f
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-16 22:39:40 +0530

    Git AutoPush

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
-echo "I am redis"
+#!/bin/bash
+
+
+USER_ID=$(id -u)
+COMPONENT=redis
+LOGFILE="/tmp/${COMPONENT}.log"
+
+if [ $USER_ID -ne 0 ] ; then
+echo -e "\e[32m script is executed by the root user or with a sudo privilage \e[0m \n \t Example : sudo bash wrapper.sh ${COMPONENT}"
+exit 1
+
+fi
+stat() {
+if [ $1 -eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else 
+  echo -e "\e[31m failure \e[0m"
+  fi
+}
+echo -e "\e[34m configuring ${COMPONENT}.......! \e[0m"
+echo -e  -n "configuring ${COMPONENT} repo :"
+curl -L https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/redis.repo -o /etc/yum.repos.d/${COMPONENT}.repo
+stat $?
+
+echo -n "Installing ${COMPONENT} :"
+yum install redis-6.2.12 -y  &>> ${LOGFILE}
+stat $? 
+
+echo -n "Enabling the ${COMPONENT} visibility:"
+sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/${COMPONENT}.conf
+stat $?
+ 
+echo -n "Starting the ${COMPONENT} :"
+systemctl daemon-reload    &>> ${LOGFILE}
+systemctl enable ${COMPONENT}    &>> ${LOGFILE}
+systemctl start ${COMPONENT}     &>> ${LOGFILE}
+stat $?
+
+echo -e "\e[32m ${COMPONENT} installation is completed \e[0m \n" 
+ 
+
