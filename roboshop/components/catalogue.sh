84c45f0c54e5f578a4a91b8edf06ec061337bdcd
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-16 19:14:24 +0530

    Git AutoPush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -47,3 +47,8 @@ echo -n "Changing the ownership :"
 mv ${COMPONENT}-main ${COMPONENT}
 chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
 stat $?
+
+echo -n "Generating the ${COMPONENT} artifacts :"
+cd /home/${APPUSER}/${COMPONENT}/
+npm install   &>> ${LOGFILE}
+stat $?
