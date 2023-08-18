48c4c8c76919676def7cb11f776407a10cae58bc
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-19 00:29:11 +0530

    Git AutoPush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -68,6 +68,7 @@ echo -n "Starting the ${COMPONENT} service :"
 systemctl daemon-reload &>> ${LOGFILE}
 systemctl enable ${COMPONENT} &>> ${LOGFILE}
 systemctl restart ${COMPONENT} &>> ${LOGFILE}
+echo -e "\e[35m ${COMPONENT} Installation is Completed \e[0m \n"
 
 
 
