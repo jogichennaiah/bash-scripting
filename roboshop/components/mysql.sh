1e4f9bcc9fc9171211ba995b086a41b55297a746
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-19 17:46:07 +0530

    Git AutoPush

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -23,7 +23,15 @@ echo -n " Extracting the default mysql password :"
 DEFAULT_ROOT_PASSWORD=$( grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
 stat $?
 
+
+#this should happen only once two for the first time , when runs for the second time ,jobs fails.
+#we need to ensure that this runs only once.
+
+echo "show database;" | mqsql -uroot -pRoboShop@1 &>> ${LOGFILE}
+
+if [ $? -ne 0 ]; then
 echo -n "Performing default password reset of root account :"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>> ${LOGFILE}
 stat $?
 
+fi
