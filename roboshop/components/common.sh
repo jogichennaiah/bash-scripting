9ba4abbdb1c23ae6869baa640e9ad3092feb05f2
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-19 15:35:15 +0530

    Git AutoPush

---
+++ roboshop/components/common.sh
@@ -0,0 +1,87 @@
+# All the common function will declared here . Rest of the components will be sourcing the function from this file
+
+
+
+LOGFILE="/tmp/${COMPONENT}.log"
+APPUSER=roboshop
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ] ; then
+echo -e "\e[32m script is executed by the root user or with a sudo privilage \e[0m \n \t Example : sudo bash wrapper.sh ${COMPONENT}"
+exit 1
+fi
+
+stat() {
+if [ $1 -eq 0 ]; then
+echo -e "\e[32m success \e[0m"
+else 
+  echo -e "\e[31m failure \e[0m"
+  fi
+}
+
+#Function to create a useraccount
+CREATE_USER() {
+id ${APPUSER}  &>> ${LOGFILE}
+if [ $? -ne 0 ] ; then
+   echo -n "Creating Application user account :"
+  useradd roboshop  
+stat $?
+fi
+
+}
+
+DOWNLOAD_AND_EXTRACT() {
+
+echo -n "Downloading the ${COMPONENT} :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
+
+cd /home/${APPUSER}/
+rm -rf ${COMPONENT}  &>> ${LOGFILE}
+unzip -o /tmp/${COMPONENT}.zip    &>> ${LOGFILE}
+ stat $?
+
+echo -n "Changing the ownership :"
+mv ${COMPONENT}-main ${COMPONENT}
+chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
+stat $?
+
+}
+
+CONFIG_SVC() {
+echo -n "Configuring the ${COMPONENT} system file :"
+sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.in/' /home/${APPUSER}/${COMPONENT}/systemd.service
+mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+stat $?
+
+echo -n "Starting the ${COMPONENT} service :"
+systemctl daemon-reload &>> ${LOGFILE}
+systemctl enable ${COMPONENT} &>> ${LOGFILE}
+systemctl restart ${COMPONENT} &>> ${LOGFILE}
+stat $?
+
+}
+
+
+#Declaring the NoDEJS function
+NODEJS() {
+    echo -e "\e[34m configuring ${COMPONENT}.......! \e[0m"
+
+echo -e  -n "configuring ${COMPONENT} repo :"
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -    &>> ${LOGFILE}
+stat $?
+
+echo -n "Installing Node Js :"
+yum install nodejs -y &>> ${LOGFILE}
+stat $? 
+
+CREATE_USER    #calls CREATE_USER function that creates user account
+DOWNLOAD_AND_EXTRACT # Download and extract the component
+
+}
+
+echo -n "Generating the ${COMPONENT} artifacts :"
+cd /home/${APPUSER}/${COMPONENT}/
+npm install    &>> ${LOGFILE}
+stat $?
