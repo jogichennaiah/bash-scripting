bcc47e6457b9e7d9a0f7d7787479df7c0c4cd11c
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-20 19:38:20 +0530

    Git AutoPush

--- roboshop/components/payment.sh
+++ roboshop/components/payment.sh
@@ -5,7 +5,7 @@ COMPONENT=catalogue
 # this is how we import the functions that are declared in a different file using source
 source components/common.sh
 
-
+PYTHON     #Installing python
 echo -e "\e[35m ${COMPONENT} Installation is Completed \e[0m \n"
 
 
