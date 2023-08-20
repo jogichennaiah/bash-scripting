9febd11f30058413344f822ec73e39a1872473b5
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-20 11:18:01 +0530

    Git AutoPush

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -46,9 +46,9 @@ unzip /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
 stat $?
 
 echo -n "updating the Backend Component in the reverse proxy file :"
-for component is catalogue user cart ;do 
+for component in catalogue user cart ;do 
    sed -i -e "/${component}/s/localhost/${component}.roboshop.in/" /etc/nginx/default.d/roboshop.conf
-   
+
 
 echo -n "Restarting  ${COMPONENT} :"
 systemctl daemon-reload  &>> ${LOGFILE}
