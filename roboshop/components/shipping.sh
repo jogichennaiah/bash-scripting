c228c0e6a20912b8a3f6c2fe9493532f6aca15eb
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-20 12:18:31 +0530

    Git AutoPush

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -4,6 +4,6 @@ COMPONENT=shipping
 # this is how we import the functions that are declared in a different file using source
 source components/common.sh
 
-NODEJS           # calling node js function
+JAVA         # calling node js function
 
 echo -e "\e[35m ${COMPONENT} Installation is Completed \e[0m \n"
