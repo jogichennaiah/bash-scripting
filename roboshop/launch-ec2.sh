a1fe9b116139a4aedb6e5a55774d10f80b1a2632
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-26 20:04:59 +0530

    Git Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 COMPONENT=$1
+
+if [ -z $1 ]; then
+   echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n\n\t"
+   echo -e "\e[35m Ex Usage $ bash launch-ec2 \e[0m"
+   exit 1
+fi
+
 AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t3.micro"
 SG_ID="sg-0c848594407d0f2a8"
