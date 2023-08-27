47edfecd5b072ecc6ec7399da1cacebcf081f899
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-27 10:27:38 +0530

    Git Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -11,6 +11,8 @@ fi
 AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t3.micro"
 SG_ID="sg-0c848594407d0f2a8"
-aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" 
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq 'Instance[].PrivateIpAddress')
 
-aws ec2 run-instances --image-id ami-0c1d144c8fdd8d690 --count 1 --instance-type t3.micro  --security-group-ids sg-0c848594407d0f2a8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=lab}]" 
+echo "Private Ip Address of the $COMPONENT is $PRIVATEIP"
+
+
