bbfd00b310844fb6f226016a54ea73378ad5229c
jogichennaiah <chennaiahjogi394@gmail.com>
2023-08-28 23:44:25 +0530

    Git Auto Push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 COMPONENT=$1
+ENV=$2
 HOSTEDZONEID="Z00893932N2EBZOMI4T0O"
 INSTANCE_TYPE="t3.micro"
 
-if [ -z $1 ]; then
+if [ -z $1 ] || [ -Z $2 ] ; then
    echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \n \t"
    echo -e "\e[35m Ex Usage $  \n\t\t bash launch-ec2 shipping "
    exit 1
@@ -18,12 +19,12 @@ SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=b55-al
 
 
 echo -e " ***** Creating \e[35m ${COMPONENT} \e[32m Server Is In Progress ****** "
-PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --count 1 --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-echo -e "Private Ip Address of the $COMPONENT is $PRIVATEIP\n\n"
+echo -e "Private Ip Address of the $COMPONENT-${ENV} is $PRIVATEIP\n\n"
 echo -e "Creating DNS Record of ${COMPONENT} :"
 
-sed -e "s/COMPONENT/${COMPONENT}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 echo -e "\e[36m ***** Creating DNS Record for the $COMPONENT has completed ***** \e[0m \n\n"
